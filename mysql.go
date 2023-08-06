7a3a3c1a44e0b6d903fe5bfd825bf7e2c145bc0d
CC11001100 <CC11001100@qq.com>
2023-08-06 15:19:27 +0800

    init

---
+++ mysql.go
@@ -0,0 +1,30 @@
+package locks
+
+import (
+	"context"
+	mysql_storage "github.com/storage-lock/go-mysql-storage"
+	"github.com/storage-lock/go-storage"
+	storage_lock "github.com/storage-lock/go-storage-lock"
+)
+
+// NewMySQLStorageLock 高层API，使用默认配置快速创建基于MySQL的分布式锁
+func NewMySQLStorageLock(ctx context.Context, lockId string, dsn string) (*storage_lock.StorageLock, error) {
+	connectionManager := mysql_storage.NewMySQLConnectionManagerFromDSN(dsn)
+	storageOptions := &mysql_storage.MySQLStorageOptions{
+		ConnectionManager: connectionManager,
+		TableName:         storage.DefaultStorageTableName,
+	}
+
+	s, err := mysql_storage.NewMySQLStorage(ctx, storageOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	lockOptions := &storage_lock.StorageLockOptions{
+		LockId:               lockId,
+		LeaseExpireAfter:     storage_lock.DefaultLeaseExpireAfter,
+		LeaseRefreshInterval: storage_lock.DefaultLeaseRefreshInterval,
+		//VersionMissRetryTimes: storage_lock.DefaultVersionMissRetryTimes,
+	}
+	return storage_lock.NewStorageLockWithOptions(s, lockOptions)
+}
